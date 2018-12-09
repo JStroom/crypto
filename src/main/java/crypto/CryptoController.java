ef3fc402087f1fc16b1889479b10a8dcab141cf4
Jeroen Stroom <jeroen.stroom@casema.nl>
2018-12-09 10:30:51 +0100

    update fxml

--- src/main/java/crypto/CryptoController.java
+++ src/main/java/crypto/CryptoController.java
@@ -1,4 +1,28 @@
 package crypto;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
 public class CryptoController {
+
+    @FXML
+    private Text buttonDecypher;
+
+    @FXML
+    private Text buttonEncypher;
+
+    @FXML
+    private TextArea textAreaUnencrypted;
+
+    @FXML
+    private TextArea textAreaPlainText;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        System.out.println("button pressed");
+        //actiontarget.setText("Sign in button pressed");
+    }
+
 }
