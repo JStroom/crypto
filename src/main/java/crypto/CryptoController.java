7d81e7197e7fc8ca76139edc2bf72f7f14ec2e72
jeroenstroom <jeroen.stroom@casema.nl>
2018-12-19 13:19:11 +0100

    1st stage controller

--- src/main/java/crypto/CryptoController.java
+++ src/main/java/crypto/CryptoController.java
@@ -1,23 +1,21 @@
 package crypto;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.text.Text;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 
-public class CryptoController {
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 
-    @FXML
-    private Text buttonDecypher;
-
-    @FXML
-    private Text buttonEncypher;
-
-    @FXML
-    private TextArea textAreaUnencrypted;
+public class CryptoController implements Initializable {
 
     @FXML
-    private TextArea textAreaPlainText;
+    private TableView tableViewPlainText;
 
     @FXML
     protected void handleSubmitButtonAction(ActionEvent event) {
@@ -25,4 +23,19 @@ public class CryptoController {
         //actiontarget.setText("Sign in button pressed");
     }
 
+    public void initTableModel() {
+        Character[] characters = {'a', 'b'};
+        List<Character> plainCipherList = Arrays.asList(characters);
+        ObservableList<Character> plainCiphers = FXCollections.observableList(plainCipherList);
+        tableViewPlainText.setItems(plainCiphers);
+//        tableViewPlainText.setText("mytext");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Character[] characters = {'a', 'b'};
+        List<Character> plainCipherList = Arrays.asList(characters);
+        ObservableList<Character> plainCiphers = FXCollections.observableList(plainCipherList);
+        //tableViewPlainText.setItems(plainCiphers);
+    }
 }
